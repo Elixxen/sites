939b868fd28151dce7b57cd092a7f9839b1c6086
Elixxen <38716900+Elixxen@users.noreply.github.com>
2018-04-25 09:38:18 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,26 +1,14 @@
 <?php
 
 if(isset($_POST['Submit'])) {
-    
 	$name = $_POST['name'];
-    
 	$course = $_POST['course'];
-    
 	$message = $_POST['message'];
     
-    
-    
-
 	$mailTo = "elixander.chamoun@alingsas.se";
-    
 	$headers = "From: Site Form Submission";
-    
-    
 
 	mail($mailTo, $course, $message, $headers);
-    
 	header("Location: index.php?mailsend");
-
 }
-
 ?>
